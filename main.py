b884790b4fcadd30df9adfa80189cd92fd54a8cd
demiapollo <apollodemirel67@gmail.com>
2023-11-13 19:03:56 -0800

    mail merge completed

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+PLACEHOLDER = "[name]"
 
 # open invited names
-with open("Input/Names/invited_names.txt", "r") as invited_names:
-    first_invited = str(invited_names.readlines(1)).strip()
-    print(first_invited)
+with open("Input/Names/invited_names.txt", "r") as names_file:
+    names = names_file.readlines()
+    print(names)
 
 
 # Open the starting letter
-with open("Input/Letters/starting_letter.txt", "r") as letter:
-    first_line = str(letter.readlines(1))
-    changed_name = first_line.replace("name", first_invited)
-    print(first_line)
+with open("Input/Letters/starting_letter.txt", "r") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"Output/ReadytoSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
 
 
 
